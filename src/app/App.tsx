08901709aae8de4e08ccf74edc4c3466edc03b88
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2022-10-17 08:59:34 +0400

    change import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Suspense } from "react"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import "app/styles/index.scss"
-import { AudioPage } from "pages/AudioPage"
-import { HomePage } from "pages/HomePage"
 import { useTheme } from "app/providers/ThemeProvider"
 import { AppRouter } from "./providers/Router/ui/AppRouter"
-import { Navbar } from "widgets/navbar/ui/Navbar"
+import { Navbar } from "widgets/navbar/"
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
